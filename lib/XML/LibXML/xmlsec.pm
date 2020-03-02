e81153385a79e70e10dee7c1fe2291482b2c92c6
Erich Strelow <esf@moller.cl>
2020-03-01 22:19:57 -0300

    At last hello world

--- lib/XML/LibXML/xmlsec.pm
+++ lib/XML/LibXML/xmlsec.pm
@@ -89,7 +89,7 @@ sub signdoc() {
       $id_node=$doc->documentElement->nodeName;
    }
 
-   my $r=$self->XmlSecSignDoc($doc,$id_attr,$id_node,$id);
+   my $r=$self->XmlSecSignDoc($doc,$self->{_keymgr},$id_attr,$id_node,$id);
 
    return $doc;
 }
